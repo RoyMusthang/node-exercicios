b4ed0b9dd67f4d1a284a87bf8a2594887b1bcb50
Josué Lobo <darksiders20005@gmail.com>
2022-01-25 01:08:08 -0300

    feat: adicionado as dependencias e configurando variaveis de ambiente

--- cep-lookup/index.js
+++ cep-lookup/index.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 
-
+const PORT = process.env.PORT;
 const app = express();
 
 app.get('/ping', (_req, res) => {
-  res.status(200).json({ menssage: "pong!"})
+  res.status(200).json({ message: "pong!"})
 })
 
-app.listen(3000, () => {
+app.get('/cep/:cep', (req, res) => {
+
+})
+
+app.listen(PORT, () => {
   console.log("Online")
 });
